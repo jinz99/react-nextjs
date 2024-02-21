539dc4c60f71f3070a0e61135f4103abc1b8fd4a
jz <278308830@qq.com>
2024-02-21 09:53:59 +0800

    tt

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,10 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
-import { inter } from '@/app/ui/fonts';
+import { lusitana } from '@/app/ui/fonts';
 
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className={`${inter.className} antialiased flex h-screen flex-col md:flex-row md:overflow-hidden`}>
+    <div className={`${lusitana.className} antialiased flex h-screen flex-col md:flex-row md:overflow-hidden`}>
       <div className="w-full flex-none md:w-64">
         <SideNav />
       </div>
